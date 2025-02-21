b772e9f47279caee968dc267b771e8d672a1e3f4
nikol4e2 <arsovnikola1@gmail.com>
2025-02-21 17:10:21 +0100

    intailize project

---
+++ QuickKick/src/main/java/com/example/quickkick/QuickKickApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickkick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickKickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickKickApplication.class, args);
+    }
+
+}
---
+++ QuickKick/src/test/java/com/example/quickkick/QuickKickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickkick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickKickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
