60ab52ecc599c50a8b6459645149bc89e136cb9f
nikol4e2 <arsovnikola1@gmail.com>
2025-02-27 20:59:45 +0100

    added controller for player

--- QuickKick/src/main/java/com/example/quickkick/service/impl/PlayerServiceImpl.java
+++ QuickKick/src/main/java/com/example/quickkick/service/impl/PlayerServiceImpl.java
@@ -8,18 +8,19 @@ import com.example.quickkick.model.exceptions.TeamNotFoundException;
 import com.example.quickkick.repository.PlayerRepository;
 import com.example.quickkick.repository.TeamRepository;
 import com.example.quickkick.service.PlayerService;
+import com.example.quickkick.service.TeamService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
 
     private final PlayerRepository playerRepository;
-    private final TeamRepository teamRepository;
+    private final TeamService teamService;
 
 
-    public PlayerServiceImpl(PlayerRepository playerRepository, TeamRepository teamRepository) {
+    public PlayerServiceImpl(PlayerRepository playerRepository, TeamService teamService) {
         this.playerRepository = playerRepository;
-        this.teamRepository = teamRepository;
+        this.teamService = teamService;
     }
 
     @Override
@@ -29,8 +30,11 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public Player addPlayer(String firstName, String lastName,String teamName) {
-        Team team = teamRepository.findByName(teamName).orElseThrow(TeamNotFoundException::new);
-        return playerRepository.save(new Player(firstName,lastName,team));
+        Team team = teamService.findByName(teamName).orElseThrow(TeamNotFoundException::new);
+
+        Player player=new Player(firstName,lastName,team);
+        teamService.addPlayerToTeam(teamName,player);
+        return playerRepository.save(player);
     }
 
     @Override
