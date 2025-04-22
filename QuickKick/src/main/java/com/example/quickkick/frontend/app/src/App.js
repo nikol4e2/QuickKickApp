8d3ff7bf24ad98b802afa8301b372e98a3346ec2
nikol4e2 <arsovnikola1@gmail.com>
2025-04-22 22:25:43 +0200

    added contact page

--- QuickKick/src/main/java/com/example/quickkick/frontend/app/src/App.js
+++ QuickKick/src/main/java/com/example/quickkick/frontend/app/src/App.js
@@ -5,7 +5,8 @@ import Navbar from "./components/nav/Navbar";
 import Home from "./components/homeComponents/home/Home";
 import Footer from "./components/footer/Footer";
 import AboutUs from "./components/aboutUs/aboutUs";
-
+import Registration from "./components/registration/Registration";
+import Contact from "./components/contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,8 @@ function App() {
       <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/about-us" element={<AboutUs></AboutUs>}></Route>
+          <Route path="/registration" element={<Registration></Registration>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
       </Routes>
         <Footer/>
     </div>
