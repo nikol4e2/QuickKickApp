2fa71a2aa1ac1b085a0883a8464772eacacd0cc0
nikol4e2 <arsovnikola1@gmail.com>
2025-04-22 22:56:34 +0200

    added gallery page

--- QuickKick/src/main/java/com/example/quickkick/frontend/app/src/App.js
+++ QuickKick/src/main/java/com/example/quickkick/frontend/app/src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/footer/Footer";
 import AboutUs from "./components/aboutUs/aboutUs";
 import Registration from "./components/registration/Registration";
 import Contact from "./components/contact/Contact";
+import Gallery from "./components/gallery/Gallery";
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about-us" element={<AboutUs></AboutUs>}></Route>
           <Route path="/registration" element={<Registration></Registration>}></Route>
           <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/photos" element={<Gallery></Gallery>}></Route>
       </Routes>
         <Footer/>
     </div>
